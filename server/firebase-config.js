1dd285d490a8c5ac09e4c48b289047825c812c37
kurtjacob22 <kurtjacob.urquico@gmail.comb>
2022-10-31 02:19:16 +0800

    added delete functions

--- server/firebase-config.js
+++ server/firebase-config.js
@@ -1,13 +1,5 @@
-const {
-  initializeApp,
-  applicationDefault,
-  cert,
-} = require("firebase-admin/app");
-const {
-  getFirestore,
-  Timestamp,
-  FieldValue,
-} = require("firebase-admin/firestore");
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 const firebase = require("firebase-admin");
 
 const serviceAccount = require("./haribon-e-wall-firebase-adminsdk-9q2vr-cdc71a0b9f.json");
@@ -25,12 +17,75 @@ const writePost = async (publisher, post, isAnonymous, callback) => {
       isAnonymous: isAnonymous,
     })
     .then(() => {
-      message = "success";
+      res = "Success";
     })
-    .catch((error) => {
-      message = "failed";
+    .catch((err) => {
+      res = err.message;
     });
-  callback(message);
+  callback(res);
 };
 
+const addComment = async (
+  commenter,
+  reply,
+  isAnonymous,
+  whatPost,
+  callback
+) => {
+  const docRef = db.collection("posts").doc(whatPost).collection("replies");
+  await docRef
+    .add({
+      commenter: commenter,
+      reply: reply,
+      time: firebase.firestore.FieldValue.serverTimestamp(),
+      isAnonymous: isAnonymous,
+    })
+    .then(() => {
+      res = "Success";
+    })
+    .catch((err) => {
+      res = err.message;
+    });
+  callback(res);
+};
+
+const deletePost = async (documentId, collectionPath) => {
+  await db.collection(collectionPath).doc(documentId).delete();
+};
+
+const deleteComment = async (postDocumentId, replyDocumentId) => {
+  await db
+    .collection("posts")
+    .doc(postDocumentId)
+    .collection("replies")
+    .doc(replyDocumentId)
+    .delete();
+};
+
+// const readAllPost = async () => {
+//   const docRef = db.collection("posts");
+//   const snapshot = await docRef.get();
+
+//   snapshot.forEach((posts) => {
+//     callback = (data) => {
+//       data = posts;
+//       return data;
+//     };
+//     // console.log(
+//     //   posts.id,
+//     //   "=>",
+//     //   posts._fieldsProto.post.stringValue,
+//     //   "by: ",
+//     //   posts._fieldsProto.publisher.stringValue
+//     // );
+//   });
+//   // console.log(data);
+//   // console.log(data.length);
+// };
+
 module.exports.writePost = writePost;
+module.exports.addComment = addComment;
+// module.exports.readAllPost = readAllPost;
+module.exports.db = db;
+module.exports.deletePost = deletePost;
+module.exports.deleteComment = deleteComment;
--- server/server.js
+++ server/server.js
@@ -2,13 +2,68 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
-const { writePost } = require("./firebase-config");
+const {
+  writePost,
+  db,
+  deleteDocument,
+  addComment,
+  deleteComment,
+} = require("./firebase-config");
 
 // console.log(process.env.DATABASE_URL);
-app.get("/", (req, res) => {
-  writePost("Kurt", "This is a message", false, (message) => {
-    return res.send(message);
+app.get("/writePost", async (req, res) => {
+  await writePost("Kurt", "This is a message", false, (postRes) => {
+    res.send(postRes);
   });
+});
+
+app.get("/addComment", async (req, res) => {
+  await addComment(
+    "Kurt",
+    "This is a reply",
+    false,
+    "GN0MfmMsraDiW6sV4esv",
+    (comRes) => {
+      res.send(comRes);
+    }
+  ).catch((err) => {
+    res.send(err.message);
+  });
+});
+
+app.get("/readAllPost", async (req, res) => {
+  const data = [];
+  const docRef = db.collection("posts");
+  const snapshot = await docRef.get();
+  snapshot.forEach((post) => {
+    data.push(post);
+  });
+
+  res.send(JSON.stringify(data));
+  // TODO: Save data to the local storage
+});
+
+app.get("/deletePost", async (req, res) => {
+  const id = "UfkvSuNGBcYuKGC9aOAV";
+  await deleteDocument(id, "posts")
+    .then(() => {
+      res.send("A document with ID: " + id + " has been deleted");
+    })
+    .catch((err) => {
+      res.send(err.message);
+    });
+});
+
+app.get("/deleteComment", async (req, res) => {
+  const postId = "GN0MfmMsraDiW6sV4esv";
+  const commentId = "R8V3uaa5Ycr9BmTfnOLC";
+  await deleteComment(postId, commentId)
+    .then(() => {
+      res.send("A document with ID: " + commentId + " has been deleted");
+    })
+    .catch((err) => {
+      res.send(err.message);
+    });
 });
 
 app.listen(PORT, () => {
